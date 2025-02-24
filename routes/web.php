653de9debec9b8e541429b83f3f9f1d9d8c96720
Pramudya Surya Anggara Putra <prasuatra@gmail.com>
2025-02-24 10:46:22 +0700

    Add UserController, UserModel, and user view; create migration for m_level and update LevelSeeder

--- routes/web.php
+++ routes/web.php
@@ -5,6 +5,7 @@ use App\Http\Controllers\LevelController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ProductCategoryController;
 use App\Http\Controllers\SalesController;
+use App\Http\Controllers\UserController;
 
 /*
 |--------------------------------------------------------------------------
@@ -36,4 +37,5 @@ Route::prefix('category')->group(function () {
 
 
 Route::get('/level', [LevelController::class,'index']);
-Route::get('/kategori', [KategoriController::class, 'index'] );
+Route::get('/kategori', [KategoriController::class, 'index']);
+Route::get('/user', [UserController::class, 'index']);
