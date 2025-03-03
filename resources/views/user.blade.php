4dfa7e9ec082c281339859febd61fb81dfcf78c3
Pramudya Surya Anggara Putra <prasuatra@gmail.com>
2025-03-03 10:54:18 +0700

    Implement user management features: add user creation, update, and deletion functionality; enhance user view with action links.

--- resources/views/user.blade.php
+++ resources/views/user.blade.php
@@ -5,35 +5,29 @@
 </head>
 <body>
     <h1>Data User</h1>
+    <a href="/user/tambah">Tambah User</a>
     <table border="1" cellpadding="2" cellspacing="0">
         <tr>
-            <th>ID</th>
-            <th>Username</th>
-            <th>Nama</th>
-            <th>Level Pengguna</th>
+            <td>ID</td>
+            <td>Username</td>
+            <td>Nama</td>
+            <td>Level ID</td>
+            <td>Aksi</td>
         </tr>
-        @if($user)
-        <tr>
-            <td>{{ $user->user_id }}</td>
-            <td>{{ $user->username }}</td>
-            <td>{{ $user->nama }}</td>
-            <td>{{ $user->level_id }}</td>
-        </tr>
-        @else
-        <tr>
-            <td colspan="4">No user found</td>
-        </tr>
-        @endif
+        @foreach ($user as $d )
+        
+            <tr>
+                <td>{{ $d->user_id }}</td>
+                <td>{{ $d->username }}</td>
+                <td>{{ $d->nama }}</td>
+                <td>{{ $d->level_id }}</td>
+                <td>
+                    <a href="/user/ubah/{{ $d->user_id }}">Edit</a>
+                    <a href="/user/hapus/{{ $d->user_id }}" onclick="return confirm('Apakah anda yakin?')">Delete</a>
+                </td>
+            </tr>
+        @endforeach
+
     </table>
-    {{-- <table border="1" cellpadding="2" cellspacing="0">
-        <tr>
-            <th>Level ID</th>
-            <th>Count</th>
-        </tr>
-        <tr>
-            <td>2</td>
-            <td>{{ $user }}</td>
-        </tr>
-    </table> --}}
 </body>
 </html>
---
+++ resources/views/user_tambah.blade.php
@@ -0,0 +1,20 @@
+<body>
+    <h1>Form Tambah Data User</h1>
+    <form action="/user/tambah_simpan" method="post">
+        {{ csrf_field()}}
+
+        <label >Username</label>
+        <input type="text" name="username" placeholder="Masukkan Username">
+        <br>
+        <label >Nama</label>
+        <input type="text" name="nama" placeholder="Masukkan Nama">
+        <br>
+        <label >Password</label>
+        <input type="password" name="password" placeholder="Masukkan Password">
+        <br>
+        <label >Level ID</label>
+        <input type="text" name="level_id" placeholder="Masukkan Level ID">
+        <br><br>
+        <input type="submit" class="btn btn-success" value="Simpan Data">
+    </form>
+</body>
